6df89020848878c29936ea1aca5b868549c1e22c
Ashish Sonone <ashson07@gmail.com>
2015-01-18 15:48:30 +0530

    data collection for lxc

---
+++ scripts/run.py
@@ -0,0 +1,107 @@
+# python run.py 2 lxc apache data0
+
+#parameters 
+#1 n  - number of vms/containers
+#2 lxc/kvm
+#3 apache/mysql
+#4 foldername - where experiment output will be stored
+
+import sys
+import os
+import time
+from subprocess import call
+
+def create_lxc_containers(n, load):
+    for i in range(int(n)):
+        container_name = "temp-" + load + "-" + str(i)
+        print(container_name)
+        call(["lxc-clone",  "-o",  load + "-master",  "-n", container_name ])
+    print(str(n) + "containers for " + load + " created")
+
+def destroy_lxc_containers(n, load):
+    for i in range(int(n)):
+        container_name = "temp-" + load + "-" + str(i)
+        print(container_name)
+        call(["lxc-stop",  "-n", container_name])
+        call(["lxc-destroy",  "-n", container_name])
+    print(str(n) + "containers for " + load + " destroyed")
+
+def create_kvm_qcow_images(n, load):
+    for i in range(int(n)):
+        base = "/mnt/local/"
+        image_name = "temp-" + load + "-" + str(i) + ".qcow"
+        print(image_name)
+        call(["qemu-img",  "create", "-f", "qcow2", "-b", base + load + "UbuntuServerHardDisk.img", base + image_name])
+    print(str(n) + "containers for " + load + " created")
+
+def run_lxc(n, load):
+    call(["lxc-ls"])
+    for i in range(int(n)):
+        container_name = "temp-" + load + "-" + str(i)
+        call(["lxc-start",  "-n", container_name, "-d"])
+    time.sleep(3)
+    call(["lxc-ls"])
+    #clean up
+
+def stop_lxc(n, load):
+    for i in range(int(n)):
+        container_name = "temp-" + load + "-" + str(i)
+        command = "lxc-stop" +  " -n " +  container_name 
+        print(command)
+        call(["lxc-stop",  "-n", container_name])
+    call(["lxc-ls"])
+
+
+def experiment_lxc(n, load, folder):
+    print("stopping uksm")
+    os.system("echo 0 > /sys/kernel/mm/uksm/run")
+    os.system("cat /sys/kernel/mm/uksm/run")
+
+    folder_without_uksm = folder + "/without"
+    folder_with_uksm = folder + "/with"
+
+    print("creating folders " + folder)
+    call(["mkdir", "-p" , folder])
+    call(["mkdir", "-p" , folder_without_uksm])
+    call(["mkdir", "-p" , folder_with_uksm])
+
+    print("running lxc containers for " + load)
+    run_lxc(n, load)
+
+    time.sleep(30);
+    call(["lxc-ls"])
+    print("now collecting data without uksm")
+    #without uksm experiment for 60 seconds
+    for i in range(10):
+        data_folder = folder_without_uksm + "/" + str(i);
+        call(["mkdir", data_folder])
+        os.system("free -m > " + data_folder + "/" + "free")
+        os.system("cp /sys/kernel/mm/uksm/pages_* " + data_folder)
+        os.system("cp /proc/meminfo " + data_folder)
+        time.sleep(1)
+
+
+    print("starting uksm")
+    os.system("echo 1 > /sys/kernel/mm/uksm/run")
+    os.system("cat /sys/kernel/mm/uksm/run")
+
+    for i in range(10*2):
+        data_folder = folder_with_uksm + "/" + str(i);
+        call(["mkdir", data_folder])
+        os.system("free -m > " + data_folder + "/" + "free")
+        os.system("cp /sys/kernel/mm/uksm/pages_* " + data_folder)
+        os.system("cp /proc/meminfo " + data_folder)
+        time.sleep(1)
+    print("data collected. Now stopping the containers")
+    stop_lxc(n, load)
+    call(["lxc-ls"])
+
+if(sys.argv[2]=="lxc"):
+    experiment_lxc(sys.argv[1], sys.argv[3], sys.argv[4])
+#create_kvm_qcow_images(15, "apache")
+#create_kvm_qcow_images(15, "mysql")
+
+#create_lxc_containers(15, "apache")
+#create_lxc_containers(15, "mysql")
+#destroy_lxc_containers(15, "apache")
+#destroy_lxc_containers(15, "mysql")
