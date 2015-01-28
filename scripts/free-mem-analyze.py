b4b82f62eddb9dbde01f9864a9e4b9f08e2ad524
Ashish Sonone <ashson07@gmail.com>
2015-01-28 21:13:30 +0530

    written free-mem-analyze.py

---
+++ scripts/free-mem-analyze.py
@@ -0,0 +1,42 @@
+"""free output first two lines :-
+                 total       used       free     shared    buffers     cached
+    Mem:         24101       3526      20575          0        359        877
+"""
+
+import os, sys
+
+if(len(sys.argv) > 1):
+    base_directory = sys.argv[1] + "/"
+else:
+    base_directory = ""
+
+free_file_name = "free"
+without_directory = base_directory + "without"
+with_directory = base_directory + "with"
+
+without_files_count = len(os.listdir(without_directory))
+with_files_count = len(os.listdir(with_directory))
+
+time = 0;
+for i in range(without_files_count-1): #one file is top file
+    f = open(without_directory+"/"+str(i)+"/"+free_file_name, 'r')
+    
+    #read the second line. split and take the third token(i.e tokens[2])
+    line = f.readline()
+    line = f.readline()
+    tokens = line.split()
+    print(str(time) + " " + tokens[2])
+    time = time + 1;
+
+print("switching...............")    
+
+for i in range(with_files_count-1):
+    f = open(with_directory+"/"+str(i)+"/"+free_file_name, 'r')
+    
+    #read the second line. split and take the third token(i.e tokens[2])
+    line = f.readline()
+    line = f.readline()
+    tokens = line.split()
+    print(str(time) + " " + tokens[2])
+    time = time + 1
+
